0dfb068bfe6f8e193a9dbefdf02f6ffac677ed23
Ken Herbin <ken@kenherbin.com>
2014-03-22 20:10:30 -0400

    added conf file

---
+++ conf/config.php
@@ -0,0 +1,10 @@
+<?php
+	$config = array(
+		"server" => "localhost",
+        "newImagePath" => "newImgs/",
+        "imagePath" => "imgs/",
+        "thumbPath" => "thumbs/",
+        "uName" => "webCMS",
+        "pword" => "shoopadoop",
+        "thumbHeight" => "250");
+?>
